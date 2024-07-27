fae4136a6142f93285e4fc746dcf57bc51350bb3
Rohit Kotamsetti <rohitshetti1@outlook.com>
2024-07-27 13:31:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="live-score">Live Score</div>
+                <div className="matches">Matches</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
